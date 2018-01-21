834ea837cc8f1cf62d5384eafc1e82bdd494ebe1
QArcicault <quentin.arcicault@gmail.com$>
2018-01-21 16:16:15 +0100

    Change for iBeacon config

--- app/src/main/java/beacons/projet/polytech/fr/beaconeddystone/MainActivity.java
+++ app/src/main/java/beacons/projet/polytech/fr/beaconeddystone/MainActivity.java
@@ -17,22 +17,18 @@ import org.altbeacon.beacon.Region;
 import java.util.Collection;
 
 
-public class MainActivity extends Activity implements BeaconConsumer, RangeNotifier {
+public class MainActivity extends Activity implements BeaconConsumer {
     protected static final String TAG = "RangingActivity";
     private BeaconManager beaconManager;
-    private TextView text;
 
-    protected void onResume() {
-        super.onResume();
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         beaconManager = BeaconManager.getInstanceForApplication(this);
         beaconManager.getBeaconParsers().clear();
 
-        beaconManager.getBeaconParsers().add(new BeaconParser().setBeaconLayout(BeaconParser.EDDYSTONE_UID_LAYOUT));
-        beaconManager.getBeaconParsers().add(new BeaconParser().setBeaconLayout(BeaconParser.EDDYSTONE_TLM_LAYOUT));
-        beaconManager.getBeaconParsers().add(new BeaconParser().setBeaconLayout(BeaconParser.ALTBEACON_LAYOUT));
-        beaconManager.getBeaconParsers().add(new BeaconParser().setBeaconLayout(BeaconParser.EDDYSTONE_URL_LAYOUT));
-        beaconManager.getBeaconParsers().add(new BeaconParser().setBeaconLayout(BeaconParser.URI_BEACON_LAYOUT));
+        beaconManager.getBeaconParsers().add(new BeaconParser().setBeaconLayout("m:2-3=0215,i:4-19,i:20-21,i:22-23,p:24-24,d:25-25"));
 
         try {
             // set the duration of the scan to be 1.11 seconds
@@ -50,49 +46,29 @@ public class MainActivity extends Activity implements BeaconConsumer, RangeNotif
         }
 
         beaconManager.bind(this);
-        beaconManager.bind(this);
-    }
-
-    public void onBeaconServiceConnect() {
-        Region region = new Region("all-beacons-region", null, null, null);
-        try {
-            beaconManager.startRangingBeaconsInRegion(region);
-        } catch (RemoteException e) {
-            e.printStackTrace();
-        }
-        beaconManager.addRangeNotifier(this);
     }
 
     @Override
-    public void didRangeBeaconsInRegion(Collection<Beacon> beacons, Region region) {
-        Log.e(TAG, "beacons size = " + beacons.size());
-        for (Beacon beacon: beacons) {
-            // This is a Eddystone-UID frame
-            Identifier namespaceId = beacon.getId1();
-            Identifier instanceId = beacon.getId2();
-            Log.e(TAG, "I see a beacon transmitting namespace id: "+namespaceId+
-                    " and instance id: "+instanceId+
-                    " approximately "+beacon.getDistance()+" meters away.");
-
-            // Do we have telemetry data?
-            if (beacon.getExtraDataFields().size() > 0) {
-                long telemetryVersion = beacon.getExtraDataFields().get(0);
-                long batteryMilliVolts = beacon.getExtraDataFields().get(1);
-                long pduCount = beacon.getExtraDataFields().get(3);
-                long uptime = beacon.getExtraDataFields().get(4);
-
-                Log.e(TAG, "The above beacon is sending telemetry version "+telemetryVersion+
-                        ", has been up for : "+uptime+" seconds"+
-                        ", has a battery level of "+batteryMilliVolts+" mV"+
-                        ", and has transmitted "+pduCount+" advertisements.");
-            }
-
-        }
-    }
-
-    @Override
-    public void onPause() {
-        super.onPause();
+    protected void onDestroy() {
+        super.onDestroy();
         beaconManager.unbind(this);
+    }
+
+    @Override
+    public void onBeaconServiceConnect() {
+        beaconManager.addRangeNotifier(new RangeNotifier() {
+            @Override
+            public void didRangeBeaconsInRegion(Collection<Beacon> beacons, Region region) {
+                if (beacons.size() > 0) {
+                    Log.i(TAG, "The first beacon I see is about "+beacons.iterator().next().getDistance()+" meters away.");
+                }
+            }
+        });
+
+        try {
+            beaconManager.startRangingBeaconsInRegion(new Region("myRangingUniqueId", null, null, null));
+        } catch (RemoteException e) {
+            Log.e(TAG, "Impossible to execute the Rage Notifier.");
+        }
     }
 }
