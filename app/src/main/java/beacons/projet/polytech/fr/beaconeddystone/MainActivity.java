19bcc9923e1d6f42f74f27fff5bd5f042ea761d1
QArcicault <quentin.arcicault@gmail.com$>
2018-01-21 15:16:30 +0100

    Add tempo scan

--- app/src/main/java/beacons/projet/polytech/fr/beaconeddystone/MainActivity.java
+++ app/src/main/java/beacons/projet/polytech/fr/beaconeddystone/MainActivity.java
@@ -27,11 +27,29 @@ public class MainActivity extends Activity implements BeaconConsumer, RangeNotif
         setContentView(R.layout.activity_main);
         beaconManager = BeaconManager.getInstanceForApplication(this);
         beaconManager.getBeaconParsers().clear();
+
         beaconManager.getBeaconParsers().add(new BeaconParser().setBeaconLayout(BeaconParser.EDDYSTONE_UID_LAYOUT));
         beaconManager.getBeaconParsers().add(new BeaconParser().setBeaconLayout(BeaconParser.EDDYSTONE_TLM_LAYOUT));
         beaconManager.getBeaconParsers().add(new BeaconParser().setBeaconLayout(BeaconParser.ALTBEACON_LAYOUT));
         beaconManager.getBeaconParsers().add(new BeaconParser().setBeaconLayout(BeaconParser.EDDYSTONE_URL_LAYOUT));
         beaconManager.getBeaconParsers().add(new BeaconParser().setBeaconLayout(BeaconParser.URI_BEACON_LAYOUT));
+
+        try {
+            // set the duration of the scan to be 1.11 seconds
+            beaconManager.setForegroundScanPeriod(110l);
+            beaconManager.setForegroundBetweenScanPeriod(2000l);
+
+            // set the duration of the scan to be 1.1 seconds
+            beaconManager.setBackgroundScanPeriod(1100l);
+            // set the time between each scan to be 2 seconds
+            beaconManager.setBackgroundBetweenScanPeriod(2000l);
+
+            beaconManager.updateScanPeriods();
+        } catch (RemoteException e) {
+            e.printStackTrace();
+        }
+
+        beaconManager.bind(this);
         beaconManager.bind(this);
     }
 
@@ -47,29 +65,28 @@ public class MainActivity extends Activity implements BeaconConsumer, RangeNotif
 
     @Override
     public void didRangeBeaconsInRegion(Collection<Beacon> beacons, Region region) {
+        Log.e(TAG, "beacons size = " + beacons.size());
         for (Beacon beacon: beacons) {
-            if (beacon.getServiceUuid() == 0xfeaa && beacon.getBeaconTypeCode() == 0x00) {
-                // This is a Eddystone-UID frame
-                Identifier namespaceId = beacon.getId1();
-                Identifier instanceId = beacon.getId2();
-                Log.d(TAG, "I see a beacon transmitting namespace id: "+namespaceId+
-                        " and instance id: "+instanceId+
-                        " approximately "+beacon.getDistance()+" meters away.");
+            // This is a Eddystone-UID frame
+            Identifier namespaceId = beacon.getId1();
+            Identifier instanceId = beacon.getId2();
+            Log.e(TAG, "I see a beacon transmitting namespace id: "+namespaceId+
+                    " and instance id: "+instanceId+
+                    " approximately "+beacon.getDistance()+" meters away.");
 
-                // Do we have telemetry data?
-                if (beacon.getExtraDataFields().size() > 0) {
-                    long telemetryVersion = beacon.getExtraDataFields().get(0);
-                    long batteryMilliVolts = beacon.getExtraDataFields().get(1);
-                    long pduCount = beacon.getExtraDataFields().get(3);
-                    long uptime = beacon.getExtraDataFields().get(4);
+            // Do we have telemetry data?
+            if (beacon.getExtraDataFields().size() > 0) {
+                long telemetryVersion = beacon.getExtraDataFields().get(0);
+                long batteryMilliVolts = beacon.getExtraDataFields().get(1);
+                long pduCount = beacon.getExtraDataFields().get(3);
+                long uptime = beacon.getExtraDataFields().get(4);
 
-                    Log.d(TAG, "The above beacon is sending telemetry version "+telemetryVersion+
-                            ", has been up for : "+uptime+" seconds"+
-                            ", has a battery level of "+batteryMilliVolts+" mV"+
-                            ", and has transmitted "+pduCount+" advertisements.");
-
-                }
+                Log.e(TAG, "The above beacon is sending telemetry version "+telemetryVersion+
+                        ", has been up for : "+uptime+" seconds"+
+                        ", has a battery level of "+batteryMilliVolts+" mV"+
+                        ", and has transmitted "+pduCount+" advertisements.");
             }
+
         }
     }
 
